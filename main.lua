b56fe94da50d77f46ddfbde61377dc33c33c9d9d
Peter Rust <peter@cornerstonenw.com>
2017-12-16 16:30:25 -0800

    Converted from pong to brick/maze game

--- main.lua
+++ main.lua
@@ -8,32 +8,30 @@ keyboard_speed = 500
 hud_height = 100
 num_lives = 5
 is_paused = false
-players = {
-  { position = 'bottom', size = 0.2, controls = 'wasd' },
-  { position = 'top', size = 0.2, controls = 'arrow_keys' }--,
-  -- { position = 'left inner', size = 0.15, controls = 'mouse' },
-  -- { position = 'right inner', size = 0.15, controls = 'controller' }
-}
-last_player = nil
+viewport_x = 0
+viewport_y = 0
 
-bricks = {
-  0, 0, 0, 1, 1, 1, 0, 0, 0,
-  0, 0, 0, 1, 1, 1, 0, 0, 0,
-  0, 1, 1, 1, 1, 1, 1, 1, 0,
-  0, 0, 0, 1, 1, 1, 0, 0, 0,
-  0, 0, 0, 1, 1, 1, 0, 0, 0,
+-- other controls: 'mouse', 'controller'
+players = {
+  { position = 'bottom', x = padding, y = padding + 200, controls = 'wasd' },
+  { position = 'top', x = padding, y = padding, controls = 'arrow_keys' }
 }
-num_h_bricks = 9
-brick_height = 20
-brick_width = 75
-brick_spacing = 3
-bricks_left_margin = 300
-bricks_top_margin = 400
+
+bricks = {}
+num_h_bricks = 20
+num_w_bricks = 50
+brick_height = 60
+brick_width = 50
+brick_spacing = 0
+bricks_left_margin = 0
+bricks_top_margin = 0
+level_width = num_w_bricks * brick_width
+level_height = num_h_bricks * brick_height
 
 function getBrickXY(ix)
   ix = ix - 1 -- b/c Lua is 1-based
-  local y = math.floor(ix / num_h_bricks)
-  local x = ix % num_h_bricks
+  local y = math.floor(ix / num_w_bricks)
+  local x = ix % num_w_bricks
   return bricks_left_margin + x * (brick_width + brick_spacing), bricks_top_margin + y * (brick_height + brick_spacing)
 end
 
@@ -42,60 +40,26 @@ local ballBumpFilter = function(item, other)
 end
 
 function love.load()
-  love.graphics.setBackgroundColor(255, 244, 204)
+  love.graphics.setBackgroundColor(0, 0, 0)
   love.graphics.setColor(174, 115, 210)
   love.window.setFullscreen(true)
-  width, height = love.graphics.getDimensions()
-  height = height - hud_height
+  screen_width, screen_height = love.graphics.getDimensions()
+  screen_height = screen_height - hud_height
   world = bump.newWorld()
 
-  love.mouse.setY(height / 2)
-  love.mouse.setX(width / 2)
+  love.mouse.setY(screen_height / 2)
+  love.mouse.setX(screen_width / 2)
   love.mouse.setVisible(false)
 
   love.graphics.setFont(love.graphics.newFont(64))
 
-  left_player = nil
-  right_player = nil
-  bottom_player = nil
   for i, player in ipairs(players) do
     player.item_type = 'paddle'
     player.score = 0
     player.lives = num_lives
 
-    if player.position == 'left' then
-      player.orientation = 'vertical'
-      left_player = player
-      player.x = padding
-    elseif player.position == 'right' then
-      player.orientation = 'vertical'
-      right_player = player
-      player.x = width - padding - 10 -- paddle_width
-    elseif player.position == 'left inner' then
-      player.orientation = 'vertical'
-      player.x = padding * 2
-    elseif player.position == 'right inner' then
-      player.orientation = 'vertical'
-      player.x = width - (padding +10) * 2
-    elseif player.position == 'bottom' then
-      player.orientation = 'horizontal'
-      bottom_player = player
-      player.y = height - padding - 10 -- paddle_height
-    elseif player.position == 'top' then
-      player.orientation = 'horizontal'
-      top_player = player
-      player.y = padding
-    end
-
-    if player.orientation == 'vertical' then
-      player.y = height / 2
-      player.width = 10
-      player.height = player.size * height
-    elseif player.orientation == 'horizontal' then
-      player.x = width / 2
-      player.height = 10
-      player.width = player.size * width
-    end
+    player.height = 60
+    player.width = 50
     player.dx = 0
     player.dy = 0
 
@@ -103,47 +67,32 @@ function love.load()
   end
 
   -- add bricks
-  for i, brick in ipairs(bricks) do
-    if brick ~= 0 then
+  local num_bricks = num_h_bricks * num_w_bricks
+  print("num-w-bricks: ", num_w_bricks)
+  for i = 1, num_bricks do
+    local ix = i - 1 -- b/c Lua is 1-based
+    local brick_y = math.floor(ix / num_w_bricks)
+    local brick_x = ix % num_w_bricks
+    print("brick-x: ", brick_x)
+    local is_edge = brick_x == 0 or brick_y == 0 or brick_x == (num_w_bricks - 1) or brick_y == (num_h_bricks - 1)
+    
+    if not is_edge and math.random() > 0.6 then
       local x, y = getBrickXY(i)
       bricks[i] = {item_type = 'brick', brick_type = brick, ix = i, x = x, y = y, width = brick_width, height = brick_height}
-      brick = bricks[i]
+      local brick = bricks[i]
       world:add(brick, brick.x, brick.y, brick.width, brick.height)
+    else
+      bricks[i] = 0
     end
   end
 
-  -- add invisible walls, in absence of players
-  if not top_player then
-    local top_wall = {x = 0, y = 0}
-    world:add(top_wall, 0,0, width, 1)
-  end
-
-  if not right_player then
-    local right_wall = { x = width, y = 0 }
-    world:add(right_wall, width,0, 1, height)
-  end
-
-  if not left_player then
-    local left_wall = { x = 0, y = 0 }
-    world:add(left_wall, 0,0, 1,height)
-  end
-
-  if not bottom_player then
-    local bottom_wall = {x = 0, y = height}
-    world:add(bottom_wall, 0,height, width, 1)
-  end
-
   ball = {
-    x = width/2,
-    y = height/2
+    x = screen_width/2,
+    y = screen_height/2
   }
-  if bottom_player then
-    ball.dx = 2 * speed
-    ball.dy = -5 * speed
-  else
-    ball.dx = -5 * speed
-    ball.dy = 2 * speed
-  end
+  ball.dx = 0 -- -5 * speed
+  ball.dy = 0 -- 2 * speed
+  
   world:add(ball, ball.x, ball.y, ball_size, ball_size)  
 
   local joysticks = love.joystick.getJoysticks()
@@ -169,9 +118,6 @@ function love.update(dt)
       local brick = cols[1].other
       world:remove(brick)
       bricks[brick.ix] = 0
-      if last_player then
-        last_player.score = last_player.score + 1
-      end
     end
 
     -- change ball direction if it bounces on anything
@@ -182,144 +128,115 @@ function love.update(dt)
     if norm.y == 1 or norm.y == -1 then
       ball.dy = -ball.dy
     end
-
-      -- the paddle's vertical speed affects the ball's vert speed
-      if cols[1].other.item_type == 'paddle' then
-        local paddle = cols[1].other
-        last_player = paddle
-        if paddle.orientation == 'vertical' then
-          ball.dy = clamp(ball.dy + paddle.dy, -6 * speed, 6 * speed)
-        else
-          ball.dx = clamp(ball.dx + paddle.dx, -6 * speed, 6 * speed)
-        end
-      end
   end
 
-  -- if someone misses the ball, start over
-  if ball.x < 0 and left_player then
-    left_player.lives = left_player.lives - 1
-    if left_player.lives == 0 then
-      is_paused = true
-    else
-      ball.x = width / 2
-      ball.dy = 2 * speed
-    end
-  elseif ball.x > width and right_player then
-    right_player.lives = right_player.lives - 1
-    if right_player.lives == 0 then
-      is_paused = true
-    else
-      ball.x = width / 2
-      ball.dy = 2 * speed
-    end
-  elseif ball.y > height and bottom_player then
-    bottom_player.lives = bottom_player.lives - 1
-    if bottom_player.lives == 0 then
-      is_paused = true
-    else
-      ball.y = height / 2
-      ball.dx = 2 * speed
-    end
-  elseif ball.y < 0 and top_player then
-    top_player.lives = top_player.lives - 1
-    if top_player.lives == 0 then
-      is_paused = true
-    else
-      ball.y = height / 2
-      ball.dx = 2 * speed
-    end
-  end
   world:update(ball, ball.x, ball.y)
 
   -- move players
   for i, player in ipairs(players) do
-    local min = 0
-    local max
-
-    local prop, dprop
-    if player.orientation == 'vertical' then
-      prop = 'y'
-      dprop = 'dy'
-      method = 'getY'
-      axis = 'lefty'
-      max = height - player.height
-      neg_arrow = 'up'
-      pos_arrow = 'down'
-      neg_wasd = 'w'
-      pos_wasd = 's'
-    elseif player.orientation == 'horizontal' then
-      prop = 'x'
-      dprop = 'dx'
-      method = 'getX'
-      axis = 'leftx'
-      max = width - player.width
-      neg_arrow = 'left'
-      pos_arrow = 'right'
-      neg_wasd = 'a'
-      pos_wasd = 'd'
-    end
+    local min_x = 0
+    local min_y = 0
+    local max_x = level_width - player.width
+    local max_y = level_height - player.height
 
     if player.controls == 'controller' and joystick_1 then
-      player[dprop] = paddle_speed * joystick_1:getGamepadAxis(axis)
+      player.dx = paddle_speed * joystick_1:getGamepadAxis('leftx')
+      player.dy = paddle_speed * joystick_1:getGamepadAxis('lefty')
     elseif player.controls == 'controller_2' and joystick_2 then
-      player[dprop] = paddle_speed * joystick_2:getGamepadAxis(axis)
+      player.dx = paddle_speed * joystick_2:getGamepadAxis('leftx')
+      player.dy = paddle_speed * joystick_2:getGamepadAxis('lefty')
     elseif player.controls == 'arrow_keys' then
-      if love.keyboard.isDown(neg_arrow) then
-        player[dprop] = -keyboard_speed
-      elseif love.keyboard.isDown(pos_arrow) then
-        player[dprop] = keyboard_speed
+      if love.keyboard.isDown('left') then
+        player.dx = -keyboard_speed
+      elseif love.keyboard.isDown('right') then
+        player.dx = keyboard_speed
       else
-        player[dprop] = 0
+        player.dx = 0
+      end
+
+      if love.keyboard.isDown('up') then
+        player.dy = -keyboard_speed
+      elseif love.keyboard.isDown('down') then
+        player.dy = keyboard_speed
+      else
+        player.dy = 0
       end
     elseif player.controls == 'wasd' then
-      if love.keyboard.isDown(neg_wasd) then
-        player[dprop] = -keyboard_speed
-      elseif love.keyboard.isDown(pos_wasd) then
-        player[dprop] = keyboard_speed
+      if love.keyboard.isDown('a') then
+        player.dx = -keyboard_speed
+      elseif love.keyboard.isDown('d') then
+        player.dx = keyboard_speed
       else
-        player[dprop] = 0
+        player.dx = 0
+      end
+      
+      if love.keyboard.isDown('w') then
+        player.dy = -keyboard_speed
+      elseif love.keyboard.isDown('s') then
+        player.dy = keyboard_speed
+      else
+        player.dy = 0
       end
     elseif player.controls == 'mouse' then
-      player[dprop] = (love.mouse[method]() - player[prop]) / dt
+      player.dx = (love.mouse.getX() - player.x) / dt
+      player.dy = (love.mouse.getY() - player.y) / dt
     else
       print('Warning: controls "' .. player.controls .. '" not valid or input device not connected')
     end
 
-    local goal = player[prop] + player[dprop] * dt
-    goal = clamp(goal, min, max)
+    local goal_x = clamp(player.x + player.dx * dt, min_x, max_x)
+    local goal_y = clamp(player.y + player.dy * dt, min_y, max_y)
 
-    local actual
-    if player.orientation == 'vertical' then
-      actualX, actualY, cols, len = world:move(player, player.x, goal)
-      actual = actualY
-    elseif player.orientation == 'horizontal' then
-      actualX, actualY, cols, len = world:move(player, goal, player.y)
-      actual = actualX
-    end
-    player[prop] = actual
+    actual_x, actual_y, cols, len = world:move(player, goal_x, goal_y)
+    player.x = actual_x
+    player.y = actual_y
 
     -- not sure if this is necessary, but it seems like a healthy precaution
-    if clamp(actual, min, max) ~= actual then
-      player[prop] = clamp(actual, min, max)
+    if clamp(actual_x, min_x, max_x) ~= actual_x then
+      player.x = clamp(actual_x, min_x, max_x)
       world:update(player, player.x, player.y)
+    end
+    if clamp(actual_y, min_y, max_y) ~= actual_y then
+      player.y = clamp(actual_y, min_y, max_y)
+      world:update(player, player.x, player.y)
+    end
+
+    -- move viewport if necessary
+    if player.x > (viewport_x +  0.9 * screen_width) then
+      viewport_x = viewport_x + 10
+    elseif player.x < (viewport_x + 0.1 * screen_width) then
+      viewport_x = viewport_x - 10
+    end
+
+    if player.y > (viewport_y + 0.9 * screen_height) then
+      viewport_y = viewport_y + 10
+    elseif player.y < (viewport_y + 0.1 * screen_height) then
+      viewport_y = viewport_y - 10
     end
   end
 end
 
 function love.draw()
+  -- draw players
   for i, player in ipairs(players) do
-    love.graphics.rectangle("fill", player.x, player.y, player.width, player.height)
+    love.graphics.rectangle("fill", player.x - viewport_x, player.y - viewport_y, player.width, player.height)
   end
+
+  -- draw bricks
   for i, brick in ipairs(bricks) do
     if brick ~= 0 then
       local x, y = getBrickXY(i)
-      love.graphics.rectangle("fill", brick.x, brick.y, brick.width, brick.height)
+      love.graphics.rectangle("fill", brick.x - viewport_x, brick.y - viewport_y, brick.width, brick.height)
     end
   end
-  love.graphics.rectangle("fill", ball.x, ball.y, ball_size, ball_size)
-  love.graphics.print(players[1].lives .. ' / ' .. players[1].score, padding, height + 5)
+
+  -- draw ball
+  love.graphics.rectangle("fill", ball.x - viewport_x, ball.y - viewport_y, ball_size, ball_size)
+  
+  -- print lives/stats in HUD
+  love.graphics.print(players[1].lives .. ' / ' .. players[1].score, padding, screen_height + 5)
   if players[2] then
-    love.graphics.print(players[2].lives .. ' / ' .. players[2].score, width - padding - 150, height + 5)
+    love.graphics.print(players[2].lives .. ' / ' .. players[2].score, screen_width - padding - 150, screen_height + 5)
   end
 end
 
